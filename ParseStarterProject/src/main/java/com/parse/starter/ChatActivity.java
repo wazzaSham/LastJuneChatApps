f5aac773184a3d2ac045bc248f25443c87ad344c
doublerr69 <thedabezt@gmail.com>
2019-06-25 16:19:31 +0800

    IT'S ALIIIIIIVE

--- ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
@@ -5,17 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class ChatActivity extends AppCompatActivity {
 
     String activeUser = "";
+
+    ArrayList<String> messages = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
 
     public void sendChat (View view){
 
@@ -47,5 +61,54 @@ public class ChatActivity extends AppCompatActivity {
 
         Log.i("Info", activeUser);
 
+        ListView chatListView = (ListView) findViewById(R.id.chatListView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
+
+        chatListView.setAdapter(arrayAdapter);
+
+        ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Message");
+
+        query1.whereEqualTo("sender", ParseUser.getCurrentUser().getUsername());
+        query1.whereEqualTo("recipient", activeUser);
+
+        ParseQuery<ParseObject> query2 = new ParseQuery<ParseObject>("Message");
+
+        query2.whereEqualTo("recipient", ParseUser.getCurrentUser().getUsername());
+        query2.whereEqualTo("sender", activeUser);
+
+        List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
+
+        queries.add(query1);
+        queries.add(query2);
+
+        ParseQuery<ParseObject> query = ParseQuery.or(queries);
+
+        query.orderByDescending("createdAt");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e==null){
+                    if(objects.size() > 0){
+
+                        messages.clear();
+
+                        for (ParseObject message : objects){
+                            String messageContent = message.getString("message");
+
+                            if(!message.getString("sender").equals(ParseUser.getCurrentUser().getUsername())){
+
+                                messageContent = "> " + messageContent;
+                            }
+
+                            messages.add(messageContent);
+                        }
+
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
     }
 }
