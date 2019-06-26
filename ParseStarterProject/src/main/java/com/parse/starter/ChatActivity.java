23e7c029660ab7136adb52b9a1a69532a3ed7144
harrisonET <harrisoneliezer@gmail.com>
2019-06-26 11:51:56 +0800

    26/6-1150:ToFix(#1) Fixed & Send Btn Changed

--- ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
@@ -1,5 +1,4 @@
 package com.parse.starter;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,51 +7,34 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.parse.FindCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
 
 public class ChatActivity extends AppCompatActivity {
-
     String activeUser = "";
-
     ArrayList<String> messages = new ArrayList<>();
-
     ArrayAdapter arrayAdapter;
 
     public void sendChat (View view){
-
         final EditText chatEditText = (EditText) findViewById(R.id.chatEditText);
-
         ParseObject message = new ParseObject("Message");
-
         message.put("sender", ParseUser.getCurrentUser().getUsername());
         message.put("recipient", activeUser);
         message.put("message", chatEditText.getText().toString());
-
         final String messageContent = chatEditText.getText().toString();
-
         chatEditText.setText("");
         message.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
                 if (e == null){
-
                     messages.add(messageContent);
-
                     arrayAdapter.notifyDataSetChanged();
-
                 }
             }
         });
@@ -63,55 +45,35 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         Intent intent = getIntent();
         activeUser = intent.getStringExtra("username");
-
         setTitle("Chat with " + activeUser);
-
         Log.i("Info", activeUser);
-
         ListView chatListView = (ListView) findViewById(R.id.chatListView);
-
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
-
         chatListView.setAdapter(arrayAdapter);
-
         ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Message");
-
         query1.whereEqualTo("sender", ParseUser.getCurrentUser().getUsername());
         query1.whereEqualTo("recipient", activeUser);
-
         ParseQuery<ParseObject> query2 = new ParseQuery<ParseObject>("Message");
-
         query2.whereEqualTo("recipient", ParseUser.getCurrentUser().getUsername());
         query2.whereEqualTo("sender", activeUser);
-
         List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
-
         queries.add(query1);
         queries.add(query2);
-
         ParseQuery<ParseObject> query = ParseQuery.or(queries);
-
         query.orderByAscending("createdAt");
-
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
                 if(e==null){
                     if(objects.size() > 0){
-
                         messages.clear();
-
                         for (ParseObject message : objects){
                             String messageContent = message.getString("message");
-
                             if(!message.getString("sender").equals(ParseUser.getCurrentUser().getUsername())){
-
                                 messageContent = "> " + messageContent;
                             }
-
                             messages.add(messageContent);
                         }
-
                         arrayAdapter.notifyDataSetChanged();
                     }
                 }
