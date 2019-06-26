a87d6522119b744af3846c2a575fe2b8f04abeda
doublerr69 <thedabezt@gmail.com>
2019-06-26 14:06:31 +0800

    Persempit v7

--- ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ChatActivity.java
@@ -44,48 +44,6 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    public void toTwo(View view){
-        setContentView(R.layout.activity_chat2);
-        Intent intent = getIntent();
-        activeUser = intent.getStringExtra("username");
-        setTitle("Chat with " + activeUser);
-        Log.i("Info", activeUser);
-        ListView chatListView = (ListView) findViewById(R.id.chatListView);
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, messages);
-        chatListView.setAdapter(arrayAdapter);
-        ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Message");
-        query1.whereEqualTo("sender", ParseUser.getCurrentUser().getUsername());
-        query1.whereEqualTo("recipient", activeUser);
-        ParseQuery<ParseObject> query2 = new ParseQuery<ParseObject>("Message");
-        query2.whereEqualTo("recipient", ParseUser.getCurrentUser().getUsername());
-        query2.whereEqualTo("sender", activeUser);
-        List<ParseQuery<ParseObject>> queries = new ArrayList<ParseQuery<ParseObject>>();
-        queries.add(query1);
-        queries.add(query2);
-        ParseQuery<ParseObject> query = ParseQuery.or(queries);
-        query.orderByAscending("createdAt");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                if(e==null){
-                    if(objects.size() > 0){
-                        messages.clear();
-                        for (ParseObject message : objects){
-                            String messageContent = message.getString("message");
-                            if(!message.getString("sender").equals(ParseUser.getCurrentUser().getUsername())){
-                                messageContent = "> " + messageContent;
-                            }
-                            messages.add(messageContent);
-                        }
-                        arrayAdapter.notifyDataSetChanged();
-                    }
-                }
-            }
-        });
-        ConstraintLayout c = (ConstraintLayout) findViewById(R.id.ChatAct);
-        int width = c.getMeasuredWidth();
-        Log.i("CHAT", String.valueOf(width));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
